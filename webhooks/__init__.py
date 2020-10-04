3d1e1b5de2f1f5f8a4a1b78125cff010078d63b6
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-10-04 16:14:25 +0200

    feat(sentry): initialize sentry

--- webhooks/__init__.py
+++ webhooks/__init__.py
@@ -1,7 +1,16 @@
 # flake8: noqa
 from flask import Flask
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 from config import Config
+
+
+sentry_sdk.init(
+    dsn="https://daa332a5889744849b88ed9d3b22f2be@o453278.ingest.sentry.io/5450166",
+    integrations=[FlaskIntegration()],
+    traces_sample_rate=1.0,
+)
 
 
 app = Flask(__name__)
