84578b4fdc278f8569eb6cd35885edc0d546cf3d
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-10-04 10:18:43 +0200

    fix(tests): use the default secret key for the tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -18,7 +18,7 @@ class TestUtils:
     data = json.dumps(payload, separators=(",", ":")).encode()
 
     def test_verify_signature(self, client):
-        github_signature = "52d1787b7d4e01d53553b8ef058b86520ea07d28"
+        github_signature = "9296374267ad49bd3fb54cdbfb129da7f4009883"
         headers = {"X-Hub-Signature": f"sha1={github_signature}"}
 
         request = Response(
@@ -40,7 +40,7 @@ class TestUtils:
     def test_verify_signature_with_wrong_payload(self, client):
         data = json.dumps({"foo": "bar"}, separators=(",", ":")).encode()
 
-        github_signature = "52d1787b7d4e01d53553b8ef058b86520ea07d28"
+        github_signature = "9296374267ad49bd3fb54cdbfb129da7f4009883"
         headers = {"X-Hub-Signature": f"sha1={github_signature}"}
 
         request = Response(response=data, headers=headers, mimetype="application/json")
