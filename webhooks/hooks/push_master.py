df114326563e8bdb5f795ba4938ec83719fad187
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-10-10 19:54:11 +0200

    fix(hooks): fix the hook push_master for looking good info in the payload

--- webhooks/hooks/push_master.py
+++ webhooks/hooks/push_master.py
@@ -1,4 +1,5 @@
 import os
+import re
 import subprocess
 
 from webhooks import app
@@ -20,8 +21,20 @@ def push_master(request):
         main_branch = local_repository.setdefault(
             "main_branch", app.config["DEFAULT_BRANCH"]
         )
+
+        ref_is_deleted = payload["deleted"]
+        commiter_names = [commit["commiter"]["name"] for commit in payload["commits"]]
+        commiter_is_not_bot = (
+            True if re.finditer(r"bot", ":".join(commiter_names)) else False
+        )
+
         if repository_name == local_repository["name"]:
-            if main_branch in ref:
+            # Two cases :
+            # 1. The user push directly on master so => ref in the payload is the
+            #    main_branch and the commiter is not a bot
+            # 2. Pushed from a pull-request so => ref in the payload is not the
+            #    main_branch but the ref_is_deleted
+            if (main_branch in ref and commiter_is_not_bot) or ref_is_deleted:
                 run_args = [
                     os.path.join(app.config["CURRENT_DIR"], "update_repos.sh"),
                     "-d",
@@ -30,6 +43,8 @@ def push_master(request):
                     repository_name,
                 ]
 
+                # TODO: this part must be improve by using a config file in the
+                # repository with the commands which must be runned after the update
                 if local_repository.setdefault("django", False):
                     run_args.extend(["-t", "django"])
                     result = subprocess.run(run_args, check=True)
